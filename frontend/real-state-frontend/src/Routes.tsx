20920fa53aa02d6822e0c7d366dd242c6331de0b
Thiago Cabral Carboneri <thiago.carboneri@mindsdigital.net>
2023-04-30 14:26:11 -0300

    Implements ErrorBoundary logic

--- frontend/real-state-frontend/src/Routes.tsx
+++ frontend/real-state-frontend/src/Routes.tsx
@@ -1,36 +1,47 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import Home from './pages/home/Home';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      suspense: true, 
+      suspense: true,
       retry: false,
       onError: (error: unknown) => {
         if ((error as Error).name === 'AbortError') return;
-        console.log(error)
-        return (
-          <h1>An error has occurred during handling data, plase try again</h1>
-        )
       },
     },
   },
 });
-import Home from './pages/home/Home';
+
+function ErrorFallback() {
+  return <h1
+    style={{
+      marginTop: '50px',
+      textAlign: "center",
+      width: "100%",
+      fontSize: "6vmin"
+    }}
+  >An error has occurred during fetching data, please try again.
+  </h1>;
+}
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <Suspense fallback={<div>Loading...</div>}>
-          < Routes>
-            <Route path="/home" element={< Home />} />
-          </Routes>
-        </Suspense>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </Suspense>
+        </ErrorBoundary>
       </QueryClientProvider>
-
-    </BrowserRouter >
+    </BrowserRouter>
   )
 };
 
