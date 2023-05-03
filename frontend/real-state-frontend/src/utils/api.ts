0155aa19e8e36421ff2d926ea7d9bb552d4ac051
Thiago Cabral Carboneri <thiago.carboneri@mindsdigital.net>
2023-05-02 21:35:10 -0300

    Creates login async function

--- frontend/real-state-frontend/src/utils/api.ts
+++ frontend/real-state-frontend/src/utils/api.ts
@@ -1,5 +1,14 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL
 
+interface IResponse {
+  success: boolean;
+  status: number;
+  error: any;
+  result: {
+    token: string
+  },
+}
+
 export const submitCredentials = async (data: { email: string, password: string, username: string }) => {
   try {
     const url = `${BASE_URL}/users`;
@@ -13,6 +22,26 @@ export const submitCredentials = async (data: { email: string, password: string,
 
     return request;
 
+  } catch (error) {
+    return null
+  }
+}
+
+export const login = async (data: { email: string, password: string, username: string }): Promise<null | IResponse> => {
+  try {
+    const url = `${BASE_URL}/login`;
+    const request = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      }),
+    });
+    const response = await request.json()
+    return {
+      ...response,
+      status: request?.status
+    }
   } catch (error) {
     return null
   }
