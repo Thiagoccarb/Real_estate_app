dd77698fbe12b0217ec4915552f10ad1dedd77a7
Thiago Cabral Carboneri <thiago.carboneri@mindsdigital.net>
2023-06-08 20:47:15 -0300

    Implements logout logicin mobileSidebar

--- frontend/real-state-frontend/src/components/header/MobileSidebar.tsx
+++ frontend/real-state-frontend/src/components/header/MobileSidebar.tsx
@@ -5,12 +5,23 @@ import InfoIcon from '@mui/icons-material/Info';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import PersonIcon from '@mui/icons-material/Person';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import { useCookies } from 'react-cookie';
+import { useNavigate } from 'react-router-dom';
 
 type IProps = {
   toggleMenu: () => void;
   isMenuOpen: boolean;
 }
 export function MobileSidebar({ toggleMenu, isMenuOpen }: IProps) {
+  const navigate = useNavigate();
+  const [, , removeCookie] = useCookies(['credentials']);
+
+  const handleLogout = () => {
+    removeCookie('credentials');
+    setTimeout(() =>navigate('/home'), 1500);
+  };
+  
   return (
     <Drawer anchor='right' open={isMenuOpen} onClose={toggleMenu}>
       <List>
@@ -46,9 +57,15 @@ export function MobileSidebar({ toggleMenu, isMenuOpen }: IProps) {
         </ListItem>
         <ListItem>
           <ListItemIcon>
-            <ExitToAppIcon />
+            <LockOpenIcon />
           </ListItemIcon>
           <ListItemText primary='Login' />
+        </ListItem>
+        <ListItem onClick={handleLogout}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText primary="Sair" />
         </ListItem>
       </List>
     </Drawer>
