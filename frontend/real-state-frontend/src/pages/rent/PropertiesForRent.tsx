6d09d645a0e52f0ae7c1d10f3d8b2dbe85788267
Thiago Cabral Carboneri <thiago.carboneri@mindsdigital.net>
2023-06-11 20:10:02 -0300

    Implements PropertiesForRent logic

---
+++ frontend/real-state-frontend/src/pages/rent/PropertiesForRent.tsx
@@ -0,0 +1,69 @@
+import { Box } from '@material-ui/core';
+import { UseQueryResult, useQuery } from 'react-query';
+import { Outlet } from 'react-router-dom';
+
+import PropertyCards from '../../components/cards/PropertyCards';
+import Footer from '../../components/Footer';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
+export interface IResult {
+  id: number,
+  name: string,
+  action: string,
+  type: string,
+  created_at: string,
+  updated_at: null | string,
+  price: number,
+  bedrooms: number,
+  bathrooms: number,
+  description: string,
+  image_urls: string[]
+  address: {
+    street_name: string,
+    number: number | null,
+    cep: string
+  },
+  city: {
+    name: string,
+    state: string
+  }
+}
+
+export interface IData {
+  success: boolean,
+  error: boolean | string,
+  message: null | string,
+  next_page: null | string,
+  previous_page: null | string,
+  result: IResult[]
+}
+
+function PropertiesForRent() {
+  const abortController = new AbortController();
+  const abortSignal = abortController.signal;
+
+  const { data }: UseQueryResult<IData> = useQuery('my-data', async () => {
+    const url = `${BASE_URL}/properties`;
+    const request = await fetch(url, { signal: abortSignal });
+    const response = await request.json()
+    return response
+  });
+
+  return (
+    <>
+      <Outlet />
+      <Box
+        component="section"
+        display="flex"
+        flexDirection="column"
+        style={{ margin: '50px 20px 0 20px' }}
+      >
+        <PropertyCards title='Aluguel de imóveis' data={data?.result.filter((item) => item.action == 'rent') ?? []} />
+        <Footer />
+      </Box>
+    </>
+  )
+}
+
+export default PropertiesForRent;
